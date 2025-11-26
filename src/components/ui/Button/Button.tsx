1cb3376fa18fd3d8cf3b3191e1a09ce6e1f9b371
JYK <jinworld13@gmail.com>
2025-11-27 01:36:28 +0900

    feat: interface

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -1,17 +1,19 @@
 import React from "react";
 import styles from "./Button.module.scss";
 
+interface ButtonProps {
+  className?: string;
+  type?: "button" | "submit" | "reset";
+  onClick?: () => void;
+  disabled?: boolean;
+}
+
 const Button = (
-  props: React.PropsWithChildren<{
-    className?: string;
-    type?: "button" | "submit" | "reset";
-    onClick?: () => void;
-    disabled?: boolean;
-  }>
+  props: ButtonProps
 ) => {
   return (
     <button
-      className={`${styles.button} ${props.className}`}
+      className={`${styles.button} ${props?.className || "" }`}
       type={props.type}
       onClick={props.onClick}
       disabled={props.disabled || false}
